f5e336865353c3d9368a661a3df8c1918fa0fcfa
Matt Mower <self@mattmower.com>
2008-03-07 09:27:00 +0000

    Configuration now more or less there. Godo can install it

--- lib/godo.rb
+++ lib/godo.rb
@@ -1,3 +1,26 @@
 class Godo
   VERSION = '1.0.0'
+  LIBPATH = File.expand_path( File.dirname( __FILE__ ) )
+  
+  def self.libpath( *args )
+    args.empty? ? LIBPATH : File.join( LIBPATH, *args )
+  end
+  
+  def self.install_config
+    self.copy( libpath( 'template.yml' ), '~/.godo' )
+  end
+  
+  def self.copy( from, path, overwrite = false )
+    if File.exists?( path ) && !overwrite
+      raise "Cannot overwrite #{path}. Please delete first and try again."
+    else
+      File.open( File.expand_path( path ), "w" ) do |file|
+        file.write( File.read( from ) )
+      end
+    end
+  end
+  
+  def self.invoke( query, options )
+    
+  end
 end
