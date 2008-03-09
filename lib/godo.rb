2148122b00242c271df0a89961c122473ae3ad0f
Matt Mower <self@mattmower.com>
2008-03-09 11:31:15 +0000

    Bumped Gem version to 1.0.2

--- lib/godo.rb
+++ lib/godo.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 module Godo
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
   LIBPATH = File.expand_path( File.dirname( __FILE__ ) )
   
   # When called with no arguments this will return the path to the gem
