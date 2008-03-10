1c6ba33305b5964cdaa9ad07c8bbbd5550d0d4c7
Lee Marlow <lee.marlow@gmail.com>
2008-03-10 17:57:27 -0600

    make sure Terminal.app is the active application before sending it keystrokes

--- lib/sessions/terminal_session.rb
+++ lib/sessions/terminal_session.rb
@@ -14,6 +14,7 @@ module Godo
     def start
       if @window
         @window.frontmost.set(true)
+        Appscript::app('Terminal').activate
         Appscript::app("System Events").application_processes["Terminal.app"].keystroke("t", :using => :command_down)
         while (@window.tabs.last.busy.get)
           # we're not on the new tab yet.  wait a bit...
